1a9ac91e58e84634a59e1008eea368192875798b
BlockChainDev-a <7.bremi.2@gmail.com>
2022-09-16 20:46:25 +1000

    update

---
+++ Sender/Sender/Sender/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Stop_Transmitting = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.audioBtn = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Stop_Transmitting
+        '
+        Me.Stop_Transmitting.Location = New System.Drawing.Point(313, 119)
+        Me.Stop_Transmitting.Name = "Stop_Transmitting"
+        Me.Stop_Transmitting.Size = New System.Drawing.Size(75, 23)
+        Me.Stop_Transmitting.TabIndex = 6
+        Me.Stop_Transmitting.Text = "Stop"
+        Me.Stop_Transmitting.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(90, 56)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(298, 20)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.Text = "192.168.208.165"
+        '
+        'audioBtn
+        '
+        Me.audioBtn.Location = New System.Drawing.Point(90, 119)
+        Me.audioBtn.Name = "audioBtn"
+        Me.audioBtn.Size = New System.Drawing.Size(75, 23)
+        Me.audioBtn.TabIndex = 9
+        Me.audioBtn.Text = "Send"
+        Me.audioBtn.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(199, 142)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(485, 189)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.audioBtn)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Stop_Transmitting)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Stop_Transmitting As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents audioBtn As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Sender/Sender/Sender/Form1.vb
@@ -0,0 +1,101 @@
+﻿Option Strict On
+
+Imports NAudio.Wave
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Single
+Imports System.IO
+Imports NAudio.CoreAudioApi.MMDevice
+Imports System
+
+Public Class Form1
+
+    Dim TimeIt1 As New Stopwatch ' For testing
+    Dim TimeIt2 As New Stopwatch ' For testing
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Sender"
+        Me.Location = New Point(CInt((Screen.PrimaryScreen.WorkingArea.Width / 2) - (Me.Width / 2)), CInt((Screen.PrimaryScreen.WorkingArea.Height / 2) - (Me.Height / 2)))
+
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As EventArgs) Handles Me.FormClosing
+        Try
+
+        Catch ex As Exception
+        End Try
+        Try
+            VoxViaUDPTxClient.Close()
+            Application.Exit()
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Dim WaveData(38400) As Byte           ' Altered by input device selected in ListBox.
+    Dim NumberofChannels As Integer = 2       ' Retrieved from input device selected in ListBox.
+    Dim SamplingRate As Integer = 48000        ' Recommend always use this amount. Keep byte amount to send per second at 32000 bytes.
+
+
+    ' VoxViaUDPTx - Start VoxViaUDPRx Listener first ___________________________________________________________________________
+
+    Dim VoxViaUDPIPAddress As IPAddress
+    Dim VoxViaUDPLogicalPortNumber As Integer = 4012
+    Dim VoxViaUDPRemoteIpEndPoint As IPEndPoint
+    Dim VoxViaUDPTxClient As UdpClient
+
+    Dim BytesSent As Integer = 0
+
+    Private Sub Stop_Transmitting_Click(sender As Object, e As EventArgs) Handles Stop_Transmitting.Click
+
+        audioBtn.Enabled = True
+        Stop_Transmitting.Enabled = False
+        RecordCapture.StopRecording()
+
+    End Sub
+
+    Dim RecordCapture As New WasapiLoopbackCapture()
+    Dim filestream As FileStream
+
+    Private Sub audioBtn_Click(sender As Object, e As EventArgs) Handles audioBtn.Click
+
+        audioBtn.Enabled = False
+
+        VoxViaUDPTxClient = New UdpClient()
+        VoxViaUDPTxClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, True)
+        VoxViaUDPIPAddress = IPAddress.Parse(TextBox1.Text)
+        VoxViaUDPTxClient.Connect(VoxViaUDPIPAddress, VoxViaUDPLogicalPortNumber)
+
+        AddHandler RecordCapture.DataAvailable, AddressOf sourceStream_DataRecord
+        RecordCapture.StartRecording()
+
+    End Sub
+
+    Private Sub sourceStream_DataRecord(sender As Object, e As NAudio.Wave.WaveInEventArgs)
+
+        Dim byteRecordCountTemp As Integer = e.BytesRecorded
+        Dim newArray16Bit(byteRecordCountTemp \ 2) As Byte
+        Dim two As Short
+        Dim value As Single
+
+        TimeIt1.Restart()
+        Dim j As Integer = 0
+
+        For i = 0 To byteRecordCountTemp - 1 Step 4
+            value = (BitConverter.ToSingle(e.Buffer, i))
+            two = CType(value * Short.MaxValue, Short)
+            newArray16Bit(j) = CType(two And &HFF, Byte)
+            newArray16Bit(j + 1) = CType((two >> 8) And &HFF, Byte)
+            j = j + 2
+        Next
+
+        Buffer.BlockCopy(newArray16Bit, 0, WaveData, 0, newArray16Bit.Length)
+        ReDim Preserve WaveData(newArray16Bit.Length)
+
+        TimeIt1.Stop()
+
+        BytesSent = VoxViaUDPTxClient.Send(WaveData, WaveData.Count)
+        ReDim Preserve WaveData(38400)
+
+    End Sub
+
+End Class
